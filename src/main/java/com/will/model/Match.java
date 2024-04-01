a3cb0bc53cd6b174a72185e2c35aa6cb0e6abd42
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-04-01 22:21:44 +0500

    update Match.java: change table name and id column

--- src/main/java/com/will/model/Match.java
+++ src/main/java/com/will/model/Match.java
@@ -6,17 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.UUID;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
 @Entity
-@Table(name = "Matches", schema = "tennis")
+@Table(name = "Match", schema = "tennis")
 public class Match {
     @Id
-    @Column(name = "uuid")
-    private UUID uuid;
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     @ManyToOne
     @JoinColumn(name = "player1", referencedColumnName = "id")
     private Player player1;
