35e91d6adf82e6a5a39d292f22036c5aa5019246
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-04-02 17:50:19 +0500

    Create MatchesRepresentationService.java for retrieving data from db according to parameters

---
+++ src/main/java/com/will/service/MatchesRepresentationService.java
@@ -0,0 +1,29 @@
+package com.will.service;
+
+import com.will.dto.MatchFilter;
+import com.will.model.Match;
+import com.will.repository.MatchesRepository;
+
+import java.util.List;
+
+public class MatchesRepresentationService {
+    private final MatchesRepository matchesRepository = new MatchesRepository();
+
+    public List<Match> findAll(String name, String pageStr) {
+        int limit = 5;
+        int offset = 0;
+
+        if (pageStr != null) {
+            if (pageStr.matches("[1-9]\\d*")) {
+                int page = Integer.parseInt(pageStr) - 1;
+                if (page > 0) {
+                    offset = page * limit;
+                }
+            } else {
+                return null;
+            }
+        }
+
+        return matchesRepository.findAll(new MatchFilter(name, limit, offset));
+    }
+}
