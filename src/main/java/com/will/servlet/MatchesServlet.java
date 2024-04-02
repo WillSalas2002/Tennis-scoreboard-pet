7914e6d4c9bee294add0cb327517a92e90d3a378
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-04-02 17:51:51 +0500

    Create MatchesServlet.java: handle requests to /matches

---
+++ src/main/java/com/will/servlet/MatchesServlet.java
@@ -0,0 +1,25 @@
+package com.will.servlet;
+
+import com.will.service.MatchesRepresentationService;
+import com.will.util.JspHelper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/matches")
+public class MatchesServlet extends HttpServlet {
+    private final MatchesRepresentationService service = new MatchesRepresentationService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pageStr = req.getParameter("page");
+        String name = req.getParameter("filter_by_player_name");
+
+        req.setAttribute("matches", service.findAll(name, pageStr));
+        req.getRequestDispatcher(JspHelper.getPath("matches")).forward(req, resp);
+    }
+}
