8f520c83f3b73a0995f5a9f458882968e4949915
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-04-05 11:23:42 +0500

    feat NewMatchRepository: implement findByName method

--- src/main/java/com/will/repository/NewMatchRepository.java
+++ src/main/java/com/will/repository/NewMatchRepository.java
@@ -1,10 +1,24 @@
 package com.will.repository;
 
+import com.will.model.Player;
 import com.will.util.HibernateUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import java.util.Optional;
 
 public class NewMatchRepository {
 
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
+    public Optional<Player> findByName(String name) {
+        try (Session session = sessionFactory.getCurrentSession()) {
+            session.beginTransaction();
+            Optional<Player> player = session.createQuery("SELECT p FROM Player p WHERE name = :name", Player.class)
+                    .setParameter("name", name)
+                    .stream().findAny();
+            session.getTransaction().commit();
+            return player;
+        }
+    }
 }
