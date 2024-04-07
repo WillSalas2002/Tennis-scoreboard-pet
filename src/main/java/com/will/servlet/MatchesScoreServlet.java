dbeeac7bd353f29ef058a2dd72a321d3ea95c9fd
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-04-07 21:42:42 +0500

    feat: implement logic of doGet and doPost methods in MatchesScoreServlet

--- src/main/java/com/will/servlet/MatchesScoreServlet.java
+++ src/main/java/com/will/servlet/MatchesScoreServlet.java
@@ -1,5 +1,12 @@
 package com.will.servlet;
 
+import com.will.dto.TempMatchStorage;
+import com.will.model.Match;
+import com.will.model.MatchScoreModel;
+import com.will.model.Player;
+import com.will.service.FinishedMatchesPersistenceService;
+import com.will.service.MatchScoreCalculationService;
+import com.will.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,17 +14,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/matches-score")
 public class MatchesScoreServlet extends HttpServlet {
+    private final MatchScoreCalculationService calculationService = new MatchScoreCalculationService();
+    private final FinishedMatchesPersistenceService persistenceService = new FinishedMatchesPersistenceService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        MatchScoreModel match = TempMatchStorage.getMatchScoreModel(uuid);
+        req.setAttribute("uuid", uuid);
+        req.setAttribute("match", match);
+        req.getRequestDispatcher(JspHelper.getPath("matches-score")).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        MatchScoreModel matchScoreModel = TempMatchStorage.getMatchScoreModel(uuid);
+        int scorerId = Integer.parseInt(req.getParameter("scorer-id"));
+        calculationService.addScore(scorerId, matchScoreModel);
+        if (!matchScoreModel.isGameFinished()) {
+            doGet(req, resp);
+            return;
+        }
+        Match match = new Match(
+                new Player(matchScoreModel.getPlayer1().getId()),
+                new Player(matchScoreModel.getPlayer2().getId()),
+                new Player(matchScoreModel.getWinner().getId())
+        );
+        Match finishedMatch = persistenceService.save(match);
+        req.setAttribute("finishedMatch", finishedMatch);
+        req.getRequestDispatcher(JspHelper.getPath("matches-score")).forward(req, resp);
     }
 }
